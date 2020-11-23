a78033ed4a31f283eb5da1eb69c224608db29f6f
RamiroVal <jesusramiro_valenzuela@outlook.com>
2020-11-23 13:34:28 -0700

    Se eliminaron archivos innecesarios

--- AltaAlumnos/FormMenu.cs
+++ AltaAlumnos/FormMenu.cs
@@ -154,6 +154,7 @@ namespace AltaAlumnos
             if (formActivo != null)
             {
                 formActivo.Close();
+                lblTitulo.Text = "HOME";
             }
         }
 
